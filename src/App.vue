07fe8bf50644e217ef35251d0e2d0d2f5531b036
HCMariah <michaletmedina@outlook.com>
2024-04-19 20:43:16 -0600

    initial creation

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup="ts">
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <header>
+    <Header />
+  </header>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <h1>Budgeting 50 | 30 | 20 Rule </h1>
+
+        <p>The 50 | 30 | 20 rule recommends putting 50% of your money toward needs, 30% toward wants, and 20% toward
+            savings.</p>
+    </div>
+    <div>
+        <img src="./icons/Chart.png" alt="Chart showing the 50 | 30 | 20 rule." style="width:650px;height:500px;">
+    </div>
+
+
+
+</template>
+
+<style>
+.container {
+    text-align: center;
+    text-wrap: pretty;
+    position: fixed;
+    top: 8px;
+    left: 16px;
+}
+
+
+img {
+    position: fixed;
+    top: 20px;
+    left: 300px;
+}
+</style>
