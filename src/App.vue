7a41d7078494d605fac1b67cd7f135158c84b06d
campbell-frost <campbellsfrost@gmail.com>
2024-04-20 01:07:39 -0400

    Updated Styling

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup="ts">
 import Header from './components/Header.vue'
+import Body from './components/Body.vue'
 </script>
 
 <template>
-  <header>
+  <div class="container">
     <Header />
-  </header>
 
-
+    <Body />
+  </div>
 </template>
 
 <style>
-
+.container {
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 2rem;
+  padding-left: 2rem;
+  position: relative;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,20 @@
 <template>
-    <header>
+    <div class="center">
         <h1>Financial Freedom</h1>
         <p>50 | 30 | 20</p>
-    </header>
+    </div>
 </template>
 
 <style scoped>
-header {
-    text-align: center;
-    position: fixed;
-    top: 5%;
-    left: 45%;
-    margin-top: -40px;
-}
-
 p {
     font-size: 26px;
 }
 
 h1 {
     font-size: 35px;
+}
+
+.center{
+    text-align: center;
 }
 </style>
