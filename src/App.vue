e9ed1342d831ced59dbc64fcd3dec3501763b85c
HCMariah <michaletmedina@outlook.com>
2024-04-24 22:15:38 -0600

    relog

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
+  <script setup>
+  import Header from '../components/Header.vue';
+  import Body from '../components/Body.vue';
+  import Footer from '@/components/Footer.vue';
+  import BudgetCalculation from '../components/BudgetCalculation.vue';
+</script>
 <template>
   <v-app>
     <v-main>
+      <v-container>
+        <Header />
+        <BudgetCalculation />
+
+        <Body />
+        <Footer />
+      </v-container>
       <router-view />
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-  //
-</script>
+
+
+<style>
+.container {
+  width: 100%;
+  padding-right: 1rem;
+  padding-left: 1rem;
+  padding-bottom: 1rem;
+  text-align: center;
+}
+</style>
