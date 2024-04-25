d45828412fae2470f59b8dc25a74ab9e5fc3b17f
HCMariah <michaletmedina@outlook.com>
2024-04-24 21:47:14 -0600

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Header from '@/components/Header.vue'
 import Body from '@/components/Body.vue'
-import Footer from 'src\\components\\Footer.vue'
+import Footer from '@/components/Footer.vue';
 import BudgetCalculation from '@/components/BudgetCalculation.vue'
 </script>
 
