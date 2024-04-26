21499fd31b7637a442cc9d3496023d4a912413bc
HCMariah <michaletmedina@outlook.com>
2024-04-25 20:26:30 -0600

    e

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import Header from '@/components/Header.vue';
 import BudgetCalculation from '@/components/BudgetCalculation.vue';
 import Body from '@/components/Body.vue'
-import Footer from '../components/Footer.vue'
+import Footer from "src/components/Footer.vue"
 </script>
 
 <template>
